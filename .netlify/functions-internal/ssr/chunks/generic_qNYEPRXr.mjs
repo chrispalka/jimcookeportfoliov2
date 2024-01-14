c7dba9de7031599b752440ccdabc738387f1afa0
Chris Palka <cpalka87@gmail.com>
2024-01-14 15:11:17 -0700

    Prep deployment to netlofy

---
+++ .netlify/functions-internal/ssr/chunks/generic_qNYEPRXr.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_E22aah5V.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_hl9RVZNN.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_JF_D6rVz.mjs');
+
+export { page };
